e9a8e3d07e53ddb8da87cf64b1b47677c30924fd
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-07-19 10:38:42 +0300

    creates a class Encod and it contructor

--- src/main/java/models/Encoding.java
+++ src/main/java/models/Encoding.java
@@ -4,6 +4,11 @@ public class Encoding {
     private String input;
     private int key;
 
+    public Encoding(String input,int key){
+        this.input=input;
+        this.key=key;
+    }
+
 
 
 
